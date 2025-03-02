50c3fdc33df903378ca714733ad8ce3cf9d3ae0b
Анастасия Савицкая <112703292+Denrezit@users.noreply.github.com>
2025-03-03 00:06:46 +1000

    Add files via upload

---
+++ codes of 8 laboratory/1task-float_elem/1task-float_elem/1task-float_elem.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+double addition_negative(double massive[], int row) {
+    double addition = 0;
+    for (int i = 0; i < row; i++) {
+        if (massive[i] < 0) {
+            addition += massive[i];
+        }
+    }
+    return addition;
+}
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    const int row = 4;
+    double massive[row] = { -3.2, 4.5, -0.6, -2.3 };
+    cout << "Сумма отрицательных элементов массива {-3.2, 4.5, -0.6, -2.3}: " << addition_negative(massive, row);
+}

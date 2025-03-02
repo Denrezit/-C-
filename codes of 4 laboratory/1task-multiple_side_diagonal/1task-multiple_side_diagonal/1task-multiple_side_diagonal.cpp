a82fd7236d361e398a909a186dfe940145cd2ed2
Анастасия Савицкая <112703292+Denrezit@users.noreply.github.com>
2025-03-02 23:52:22 +1000

    Add files via upload

---
+++ codes of 4 laboratory/1task-multiple_side_diagonal/1task-multiple_side_diagonal/1task-multiple_side_diagonal.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	const int n = 5;
+	int mass[n][n] = { {3,4,1,7,3}, {2,5,5,2,9}, {5,3,1,8,2}, {2,6,1,1,2}, {8,1,1,5,5} };
+	int mass_multi = 1;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = n - 1; j >= 0; j--) {
+			mass_multi *= mass[i][j];
+			i += 1;
+
+		}
+	}
+	cout << "Результат выполнения произведения побочной диагонали: " << mass_multi;
+}
---
+++ codes of 4 laboratory/2task-row_position/2task-row_position/2task-row_position.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	const int m = 5;
+	int mass[6][6] = { {1,1,1,4,2,1},{4,2,1,2,3,2},{5,5,3,4,1,3},{3,2,4,2,1,4},{8,7,2,1,1,5},{1,8,1,2,8,3} };
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j <= m; j++) {
+			int t = mass[i][j];
+			mass[i][j] = mass[m - i][j];
+			mass[m - i][j] = t;
+		}
+
+	}
+
+	cout << "Изменённая матрица: \n";
+	for (int i = 0; i <= m; i++) {
+		for (int j = 0; j <= m; j++) {
+			cout << mass[i][j];
+		}
+		cout << endl;
+	}
+}
